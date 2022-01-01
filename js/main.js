3bef9abc41fe77bc73e42f601e5fee1bc30b0b71
Jason Sohl <jason@frozensoliddesigns.com>
2022-01-01 11:54:18 -0800

    adding fallback for intersection observer non-support

--- js/main.js
+++ js/main.js
@@ -1,27 +1,39 @@
-var observer = new IntersectionObserver(function(entries) {
-  for (entry in entries) {
-    if(entries[entry].isIntersecting === true) {
-      
-      var panel = entries[entry].target,
-          incomingPanel = panel.dataset.panel;
-      
-      if (panel !== undefined) {
-        if (!panel.classList.contains("reveal")) {
-          panel.classList = "reveal";
-        }
+if (!('IntersectionObserver' in window) ||
+    !('IntersectionObserverEntry' in window) ||
+    !('intersectionRatio' in window.IntersectionObserverEntry.prototype)) {
+    // load polyfill now
+    var sections = document.querySelectorAll(".section");
+    for (var i = 0; i < sections.length; i++) {
+      if (!sections[i].classList.contains("reveal")) {
+        sections[i].classList.add("reveal");
       }
-
-      // switch(incomingPanel) {
-      //   case 'two':
-      //       panel.classList = "reveal";
-      //     break;
-      // }
     }
-  }
+} else {
+  var observer = new IntersectionObserver(function(entries) {
+    for (entry in entries) {
+      if(entries[entry].isIntersecting === true) {
+        
+        var panel = entries[entry].target,
+            incomingPanel = panel.dataset.panel;
+        
+        if (panel !== undefined) {
+          if (!panel.classList.contains("reveal")) {
+            panel.classList = "reveal";
+          }
+        }
 
-}, { threshold: [0.25] });
+        // switch(incomingPanel) {
+        //   case 'two':
+        //       panel.classList = "reveal";
+        //     break;
+        // }
+      }
+    }
 
-observer.observe(document.querySelector("#section-1"));
-observer.observe(document.querySelector("#section-2"));
-observer.observe(document.querySelector("#section-3"));
-observer.observe(document.querySelector("#section-4"));
+  }, { threshold: [0.25] });
+
+  observer.observe(document.querySelector("#section-1"));
+  observer.observe(document.querySelector("#section-2"));
+  observer.observe(document.querySelector("#section-3"));
+  observer.observe(document.querySelector("#section-4"));
+}
